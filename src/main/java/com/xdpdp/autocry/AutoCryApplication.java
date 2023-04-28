e15596d75d0e4faeb3a212be2e807330a53ca36b
0xDpDp <132055677+0xDpDp@users.noreply.github.com>
2023-04-28 16:58:00 +0700

    init

---
+++ src/main/java/com/xdpdp/autocry/AutoCryApplication.java
@@ -0,0 +1,13 @@
+package com.xdpdp.autocry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xdpdp/autocry/AutoCryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdpdp.autocry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
